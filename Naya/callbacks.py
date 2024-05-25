0e2e5518045b11710f7b31fe1c1ae995537ac02d
barcacoty2024 <158706333+barcacoty2024@users.noreply.github.com>
2024-05-26 03:15:53 +0700

    callbacks.py

--- Naya/callbacks.py
+++ Naya/callbacks.py
@@ -60,7 +60,7 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
                 await callback_query.answer("Please note that this bot session will be of pyrogram v2", show_alert=True)
                 await generate_session(bot, callback_query.message, is_bot=True)
             elif query == "telethon_bot":
-                await callback_query.answer("Please note that this bot session will be of telethon_bot", show_alert=True)
+                await callback_query.answer("Please note that this bot session will be of telethon", show_alert=True)
                 await generate_session(bot, callback_query.message, telethon=True, is_bot=True)
             elif query == "telethon":
                 await callback_query.answer()
